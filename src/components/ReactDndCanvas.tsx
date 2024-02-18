890c3e7fa9425d0e703d83fea1e709fff95e4237
Lucas Della Bella <dellabella.lucas@gmail.com>
2024-02-17 20:54:34 -0800

    Set dep arrays on react dnd hooks

--- src/components/ReactDndCanvas.tsx
+++ src/components/ReactDndCanvas.tsx
@@ -1,33 +1,38 @@
 "use client"
 
 import React, { useState } from "react"
-import { DndProvider, useDrop } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+import { useDrop } from "react-dnd"
 
 import DraggableBox from "./DraggableBox"
 
 const Canvas: React.FC = () => {
   // Initial position for the draggable box
   const [boxPosition, setBoxPosition] = useState({
-    id: "box1",
+    id: "box",
     left: 100,
     top: 100,
   })
 
-  const [, drop] = useDrop(() => ({
-    accept: "box",
-    drop(item: { id: string; left: number; top: number }, monitor) {
-      const delta = monitor.getDifferenceFromInitialOffset()
-      if (delta) {
-        const left = Math.round(item.left + delta.x)
-        const top = Math.round(item.top + delta.y)
-        setBoxPosition({ ...boxPosition, left, top })
-      }
-    },
-  }))
+  const [, drop] = useDrop(
+    () => ({
+      accept: "box",
+      drop(item: { id: string; left: number; top: number }, monitor) {
+        const delta = monitor.getDifferenceFromInitialOffset()
+        if (delta) {
+          console.log("item", item)
+          console.log("delta", delta)
+          const left = Math.round(item.left + delta.x)
+          const top = Math.round(item.top + delta.y)
+          setBoxPosition({ ...boxPosition, left, top })
+          return undefined
+        }
+      },
+    }),
+    [boxPosition, setBoxPosition]
+  )
 
   return (
-    <div ref={drop} className="h-screen w-screen">
+    <div id="dropzone" ref={drop} className="h-screen w-screen">
       <DraggableBox
         id={boxPosition.id}
         left={boxPosition.left}
