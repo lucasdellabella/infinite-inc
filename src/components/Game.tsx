25ecfc732549ded818d503258ab002e628186f77
mgallagher1890 <mgallagher1890@gmail.com>
2024-02-23 23:39:39 -0800

    hoping to add some response caching but its not ready yet

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,12 @@
 "use client"
 
-import { useState } from "react"
-import { capitalizeWords } from "@/util/str"
-import { createClient, dbNormalizeStr } from "@/util/supabase/client"
-
-import { prompt } from "@/app/actions"
+import { setInterval } from "timers"
+import { useEffect, useState } from "react"
+import { Board, tick } from "@/util/game"
 
 export default function GameV1() {
-  const supabase = createClient()
-  const [board, setBoard] = useState<[string, string, string | null][]>([])
+  const [board, setBoard] = useState<Board>([])
+  const [ticks, setTicks] = useState<number>(0)
 
   const [inventory, setInventory] = useState(
     new Map<string, number>([
@@ -20,80 +18,13 @@ export default function GameV1() {
   const [input1, setInput1] = useState<string>("")
   const [input2, setInput2] = useState<string>("")
 
-  const onClickPrompt = async (e: MouseEvent) => {
-    if (!board || board.length === 0) return
-    const hashFn = (n1: string, n2: string) => {
-      const [name1, name2] = [n1, n2].map(dbNormalizeStr).sort()
-      return `and(name1.eq.${name1},name2.eq.${name2})`
-    }
-
-    const filter = board
-      .map(([n1, n2]) => hashFn(n1, n2))
-      .filter((x) => x)
-      .join(",")
-
-    const { data: existingCombos } =
-      (await supabase
-        ?.from("combos")
-        .select("name1,name2,res_name1")
-        .or(filter)) || {}
-
-    const comboMap = new Map(
-      existingCombos?.map(({ name1, name2, res_name1 }) => {
-        return name1 && name2
-          ? [
-              hashFn(name1, name2),
-              res_name1
-                ? capitalizeWords(res_name1).replaceAll("_", " ")
-                : null,
-            ]
-          : ["", null]
-      }) || []
-    )
-
-    const newBoard = await Promise.all(
-      board.map(async (b): Promise<[string, string, string | null]> => {
-        const [n1, n2, r1] = b
-        if (r1) return b
-        const h = hashFn(n1, n2)
-        const existingRes = comboMap.get(h) || null
-
-        if (existingRes) return [n1, n2, existingRes]
-        else {
-          const newRes = await prompt(n1, n2)
-          console.log(newRes)
-          const [, , match] = newRes.split("|")
-          return [n1, n2, match?.toString() || null]
-        }
-      })
-    )
-
-    const newInventory = new Map(inventory)
-    const indexes: number[] = []
-
-    newBoard.forEach(([n1, n2, r1], i) => {
-      const updateValue = (n: string, q: number) =>
-        newInventory.set(n, (newInventory.get(n) || 0) + q)
-
-      const check = [n1, n2]
-        .filter((x) => x !== "Punch")
-        .map((n) => {
-          if (newInventory.has(n) && (newInventory.get(n) || 0) > 0) {
-            updateValue(n, -1)
-            return true
-          } else {
-            indexes.push(i)
-            return false
-          }
-        })
-        .every((x) => x === true)
-      if (r1 && check) updateValue(r1, 1)
-    })
-    setInventory(newInventory)
-    setBoard(newBoard.filter((_, i) => indexes.indexOf(i) === -1))
+  const tickle = async () => {
+    await tick(board, setBoard, inventory, setInventory)
+    setTicks(ticks + 1)
   }
+
   const onClickAdd = async (e: MouseEvent) => {
-    if (input1 && input2 && board.length <= 10) {
+    if (input1 && input2 && board.length < 10) {
       setBoard([...board, [input1, input2, null]])
       setInput1("")
       setInput2("")
@@ -158,10 +89,10 @@ export default function GameV1() {
           ))}
       </div>
       <button
+        onClick={tickle}
         className="rounded-md bg-purple-500 px-3 py-2 text-sm font-semibold text-white hover:bg-purple-600"
-        onClick={onClickPrompt}
       >
-        Tick
+        {ticks} Tick
       </button>
     </div>
   )
---
+++ src/util/game.ts
@@ -0,0 +1,85 @@
+"use client"
+
+import { cache } from "react"
+
+import { prompt } from "@/app/actions"
+
+import { capitalizeWords } from "./str"
+import { createClient, dbNormalizeStr } from "./supabase/client"
+
+export type Board = [string, string, string | null][]
+const supabase = createClient()
+
+export async function tick(
+  board: Board,
+  setBoard: (b: Board) => void,
+  inventory: Map<string, number>,
+  setInventory: (i: Map<string, number>) => void
+) {
+  if (!board || board.length === 0) return
+  const hashFn = (n1: string, n2: string) => {
+    const [name1, name2] = [n1, n2].map(dbNormalizeStr).sort()
+    return `and(name1.eq.${name1},name2.eq.${name2})`
+  }
+
+  const filterRaw = board.map(([n1, n2]) => hashFn(n1, n2)).filter((x) => x)
+
+  const filter = Array.from(new Set(filterRaw)).sort().join(",")
+
+  const getExistingCombos = async (f: string) =>
+    await supabase?.from("combos").select("name1,name2,res_name1").or(f)
+
+  const { data: existingCombos } = (await getExistingCombos(filter)) || {}
+
+  const comboMap = new Map(
+    existingCombos?.map(({ name1, name2, res_name1 }) => {
+      return name1 && name2
+        ? [
+            hashFn(name1, name2),
+            res_name1 ? capitalizeWords(res_name1).replaceAll("_", " ") : null,
+          ]
+        : ["", null]
+    }) || []
+  )
+
+  const newBoard = await Promise.all(
+    board.map(async (b): Promise<[string, string, string | null]> => {
+      const [n1, n2, r1] = b
+      if (r1) return b
+      const h = hashFn(n1, n2)
+      const existingRes = comboMap.get(h) || null
+
+      if (existingRes) return [n1, n2, existingRes]
+      else {
+        const newRes = await prompt(n1, n2)
+        console.log(newRes)
+        const [, , match] = newRes.split("|")
+        return [n1, n2, match?.toString() || null]
+      }
+    })
+  )
+
+  const newInventory = new Map(inventory)
+  const indexes: number[] = []
+
+  newBoard.forEach(([n1, n2, r1], i) => {
+    const updateValue = (n: string, q: number) =>
+      newInventory.set(n, (newInventory.get(n) || 0) + q)
+
+    const check = [n1, n2]
+      .filter((x) => x !== "Punch")
+      .map((n) => {
+        if (newInventory.has(n) && (newInventory.get(n) || 0) > 0) {
+          updateValue(n, -1)
+          return true
+        } else {
+          indexes.push(i)
+          return false
+        }
+      })
+      .every((x) => x === true)
+    if (r1 && check) updateValue(r1, 1)
+  })
+  setInventory(newInventory)
+  setBoard(newBoard.filter((_, i) => indexes.indexOf(i) === -1))
+}
