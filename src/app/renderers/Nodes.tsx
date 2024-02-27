3df96525035a460f1a6368a655adc0fe808a2eab
Lucas Della Bella <dellabella.lucas@gmail.com>
2024-02-27 14:57:29 -0800

    Add some initial data + update renderer

--- src/app/renderers/Nodes.tsx
+++ src/app/renderers/Nodes.tsx
@@ -8,7 +8,7 @@ const Nodes = ({ nodes }: Props) => {
   const size = 100;
   return (
     <div>
-      {nodes.map(({ id, position }) => {
+      {nodes.map(({ id, emoji, name, position }) => {
         if (!position) {
           return null;
         }
@@ -17,13 +17,21 @@ const Nodes = ({ nodes }: Props) => {
           <div
             key={id}
             data-entity-id={id}
-            className="absolute left-0 top-0 cursor-pointer bg-red-900"
+            className="absolute group flex items-center py-2 px-4 left-0 top-0 select-none"
             style={{
-              width: size,
-              height: size,
+              minWidth: "60px",
+              minHeight: "20px",
               transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
             }}
-          />
+          >
+            <div className="absolute z-[-1] top-0 left-0 bg-white border border-slate-50 pointer-events-none w-full h-full rounded-md cursor-pointer opacity-0 transition-all group-hover:opacity-100"></div>
+            <span className="text-3xl cursor-pointer pointer-events-none">
+              {emoji}
+            </span>
+            <span className="text-xl ml-2 opacity-0 transition-all group-hover:opacity-100">
+              {name}
+            </span>
+          </div>
         );
       })}
     </div>
