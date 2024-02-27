8b79fae899c6b73185870b5546dd43a0b51b5b00
Joe Wang <joewang704@gmail.com>
2024-02-27 18:37:53 -0500

    Fix drag sometimes not working

--- src/app/systems/handleDrag.ts
+++ src/app/systems/handleDrag.ts
@@ -6,38 +6,40 @@ export const handleDrag = (() => {
   let targetEntityId = "";
 
   return (entities: EntitiesPayload, { input, time }: SystemArgs<any>) => {
-    const { name, payload } =
-      input.find((x) =>
+    const events =
+      input.filter((x) =>
         ["onMouseDown", "onMouseUp", "onMouseMove"].includes(x.name)
-      ) || {};
+      ) || [];
 
-    if (name === "onMouseDown") {
-      const entityId = payload?.target.getAttribute("data-entity-id");
-      targetEntityId = entityId;
-      const entity = entities.gameObjects.nodes.find(
-        ({ id }) => id === targetEntityId
-      );
-      if (entity) {
-        entity.draggable.isBeingDragged = true;
-        dragOffset = {
-          x: payload?.clientX - payload?.target.getBoundingClientRect().left,
-          y: payload?.clientY - payload?.target.getBoundingClientRect().top,
-        };
+    events.forEach(({ name, payload }) => {
+      if (name === "onMouseDown") {
+        const entityId = payload?.target.getAttribute("data-entity-id");
+        targetEntityId = entityId;
+        const entity = entities.gameObjects.nodes.find(
+          ({ id }) => id === targetEntityId
+        );
+        if (entity) {
+          entity.draggable.isBeingDragged = true;
+          dragOffset = {
+            x: payload?.clientX - payload?.target.getBoundingClientRect().left,
+            y: payload?.clientY - payload?.target.getBoundingClientRect().top,
+          };
+        }
+      } else if (targetEntityId && name === "onMouseMove") {
+        const entity = entities.gameObjects.nodes.find(
+          ({ id }) => id === targetEntityId
+        );
+        entity.position.x = payload?.pageX - dragOffset.x;
+        entity.position.y = payload?.pageY - dragOffset.y;
+      } else if (targetEntityId && name === "onMouseUp") {
+        const entity = entities.gameObjects.nodes.find(
+          ({ id }) => id === targetEntityId
+        );
+        targetEntityId = "";
+        dragOffset = { x: 0, y: 0 };
+        entity.draggable.isBeingDragged = false;
       }
-    } else if (targetEntityId && name === "onMouseMove") {
-      const entity = entities.gameObjects.nodes.find(
-        ({ id }) => id === targetEntityId
-      );
-      entity.position.x = payload?.pageX - dragOffset.x;
-      entity.position.y = payload?.pageY - dragOffset.y;
-    } else if (targetEntityId && name === "onMouseUp") {
-      const entity = entities.gameObjects.nodes.find(
-        ({ id }) => id === targetEntityId
-      );
-      targetEntityId = "";
-      dragOffset = { x: 0, y: 0 };
-      entity.draggable.isBeingDragged = false;
-    }
+    })
 
     return entities;
   };
