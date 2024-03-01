58ca5dc830aba276672f661e804da8cdf34a82a5
Lucas Della Bella <dellabella.lucas@gmail.com>
2024-02-29 21:53:18 -0800

    Merge origin/main and lucas' main

--- src/app/renderers/Nodes.tsx
+++ src/app/renderers/Nodes.tsx
@@ -45,7 +45,7 @@ const Node = ({ id, emoji, name, position }) => {
     <div
       key={id}
       data-entity-id={id}
-      className="absolute group flex items-center py-1 px-2 left-0 top-0 select-none cursor-pointer"
+      className="absolute group z-[9999999999] flex items-center py-1 px-2 left-0 top-0 select-none cursor-pointer"
       onMouseEnter={handleHoverStart}
       onMouseLeave={handleHoverEnd}
       onTouchStart={handleHoverStart}
@@ -54,7 +54,7 @@ const Node = ({ id, emoji, name, position }) => {
         transform: `translate3d(${position.x}px, ${position.y}px, 0)`,
       }}
     >
-      <div className="absolute z-[-1] top-0 left-0 bg-transparent border border-slate-900 pointer-events-none w-full h-full rounded-md cursor-pointer opacity-0 transition-all group-hover:opacity-100"></div>
+      <div className="absolute z-[-1] top-0 left-0 bg-white border border-slate-900 pointer-events-none w-full h-full rounded-md cursor-pointer opacity-0 transition-all group-hover:opacity-60"></div>
       <span className="text-3xl cursor-pointer pointer-events-none">
         {emoji}
       </span>
