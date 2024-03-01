21cd2d177ed163c6d5faca6d82cb7715b809be0c
mgallagher1890 <mgallagher1890@gmail.com>
2024-03-01 08:09:41 -0800

    add simple response cache in mem

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,38 +1,52 @@
 import axios from "axios";
 import { capitalizeFirstLetter } from "../utils/string";
 
+type Result = { name: string; emoji: string };
+
+const resCache = new Map<string, Result>();
+
 export async function combine(
   name1: string,
   name2: string
-): Promise<{ name: string; emoji: string } | null> {
+): Promise<Result | null> {
   console.log("combining", name1, name2);
+
+  const [n1, n2] = [name1, name2].sort()
 
   const host =
     process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
 
-  const url = `${host}/api/prompt?name1=${name1}&name2=${name2}`;
-  return await axios
-    .get(url)
-    .then(function (response) {
-      // handle success
-      console.log(response);
-      const res = response?.data;
-      const [name, emoji] = res || [];
+  const url = `${host}/api/prompt?name1=${n1}&name2=${n2}`;
 
-      return {
-        name: name
-          .replaceAll("_", " ")
-          .split(" ")
-          .map(capitalizeFirstLetter)
-          .join(" "),
-        emoji,
-      };
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      return null;
-    });
+  if (!resCache.has(url)) {
+    return await axios
+      .get(url)
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        const res = response?.data;
+        const [name, emoji] = res || [];
+
+        const result = {
+          name: name
+            .replaceAll("_", " ")
+            .split(" ")
+            .map(capitalizeFirstLetter)
+            .join(" "),
+          emoji,
+        };
+
+        if (resCache.size < 100) resCache.set(url, result);
+        return result;
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        return null;
+      });
+  } else {
+    return resCache.get(url) || null;
+  }
 }
 
 export default { combine };
