d9d6b850da0323eb9178b4f686da692de021543d
mgallagher1890 <mgallagher1890@gmail.com>
2024-03-22 18:37:12 -0700

    catch 504s

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -8,36 +8,39 @@ export async function combine(
   name1: string,
   name2: string
 ): Promise<Result | null> {
-  
+  try {
+    const [n1, n2] = [name1, name2].sort();
 
-  const [n1, n2] = [name1, name2].sort()
+    const host =
+      process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
 
-  const host =
-    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "";
+    const url = `${host}/api/prompt?name1=${n1}&name2=${n2}`;
 
-  const url = `${host}/api/prompt?name1=${n1}&name2=${n2}`;
+    if (!resCache.has(url)) {
+      console.log("combining", name1, name2);
+      return await axios
+        .get(url)
+        .then(function (response) {
+          // handle success
+          console.log(response);
+          const res = response?.data;
+          const [name, emoji, props] = res || [];
 
-  if (!resCache.has(url)) {
-    console.log("combining", name1, name2);
-    return await axios
-      .get(url)
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        const res = response?.data;
-        const [name, emoji, props] = res || [];
-
-        const result = {name, emoji, props}
-        if (resCache.size < 100) resCache.set(url, result);
-        return result;
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-        return null;
-      });
-  } else {
-    return resCache.get(url) || null;
+          const result = { name, emoji, props };
+          if (resCache.size < 100) resCache.set(url, result);
+          return result;
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+          return null;
+        });
+    } else {
+      return resCache.get(url) || null;
+    }
+  } catch (e) {
+    console.log("too slow!", name1, name2);
+    return null;
   }
 }
 
